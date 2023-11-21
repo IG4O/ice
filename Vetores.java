ba0490087d0ccd9b5441fcd1b2332b4873cdf8f2
IGOR <92439234+IG4O@users.noreply.github.com>
2023-11-21 19:04:37 -0300

    vetores

---
+++ Vetores.java
@@ -0,0 +1,19 @@
+public class Vetores {
+    public static void main(String[] args) {
+        
+    }   
+    
+    public static void vetorPrintInt(int [] vetor){
+        int i;
+        for (i = 0; i<vetor.length;i++){
+            System.out.printf("%d", vetor[i]);
+        }
+        System.out.println();
+    }
+
+    public static int vetorNovoInt(int[] x){
+        
+        
+        return x[0];
+    }
+}
