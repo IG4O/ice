5947c0c885287e0ac7f4b79a44e665dc8b140fbd
IGOR <92439234+IG4O@users.noreply.github.com>
2023-11-21 19:03:35 -0300

    Edição

--- Aula.java
+++
@@ -1,5 +0,0 @@
-public class Aula {
-    public static void main(String[] args){
-        
-    }
-}
--- vetores.java
+++
@@ -1,9 +0,0 @@
-public class vetores {
-    public static void vetorPrintInt(int [] vetor){
-        int i;
-        for (i = 0; i<vetor.length;i++){
-            System.out.printf("%d", vetor[i]);
-        }
-        System.out.println();
-    }
-}
