9105f799a08a65abb0caae8181bd673a2d45ac0f
IGOR <92439234+IG4O@users.noreply.github.com>
2023-11-26 18:44:52 -0300

    m

--- Matrizes.java
+++ Matrizes.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 
 public class Matrizes {
-    public static Scanner entrada = new Scanner(System.in);
+    //public static Scanner entrada = new Scanner(System.in);
     public static int linhas = 5;
     public static int colunas = 5;
-    
+
     public static void main(String[] args){
             
-        int [][] matriz;
-        int i,j, valor;
+        // int [][] matriz;
+        // int i,j, valor;
 
-        int n_linhas = 4, n_colunas = 4;
+        // int n_linhas = 4, n_colunas = 4;
 
-        matriz = new int[n_linhas][n_colunas];
+        // matriz = new int[n_linhas][n_colunas];
 
-        for (i = 0; i < n_linhas;i++){
+        // for (i = 0; i < n_linhas;i++){
 
-            System.out.println("Digite linha: ");
-            valor = entrada.nextInt();
-            i = valor;
+        //     System.out.println("Digite linha: ");
+        //     valor = entrada.nextInt();
+        //     i = valor;
 
-            for(j = 0; j < n_colunas;j++){
-                System.out.println("Digite coluna: ");
-                valor = entrada.nextInt();
-                j = valor;
-            }
-        }
+        //     for(j = 0; j < n_colunas;j++){
+        //         System.out.println("Digite coluna: ");
+        //         valor = entrada.nextInt();
+        //         j = valor;
+        //     }
+        // }
 
         System.out.println("[matrizNovaInt]");
         int[][] matrizInt = matrizNovaInt(colunas, linhas);
@@ -52,32 +52,27 @@ public class Matrizes {
 
     }
 
-    public static int [][] matrizLeitura(int n_linhas, int n_colunas){
-        int [][] matriz = new int[n_linhas][n_colunas];
-        int i, j;
-        System.out.printf("Entre com os valores da matriz", n_linhas*n_colunas);
+    // public static int [][] matrizLeitura(int n_linhas, int n_colunas){
+    //     int [][] matriz = new int[n_linhas][n_colunas];
+    //     int i, j;
+    //     System.out.printf("Entre com os valores da matriz", n_linhas*n_colunas);
 
-        for (i=0;i<n_linhas;i++){
-            for(j = 0; j<n_colunas;j++){
-                matriz [i][j] = entrada.nextInt();
-            }
-        }
-        return matriz;
-    }
+    //     for (i=0;i<n_linhas;i++){
+    //         for(j = 0; j<n_colunas;j++){
+    //             matriz [i][j] = entrada.nextInt();
+    //         }
+    //     }
+    //     return matriz;
+    // }
 
-    public static void matrizPrint(int [][]matriz){
-        for (int i = 0; i<matriz.length; i++){
-            for (int j=0; j<matriz[0].length;j++){
-                System.out.printf("%d",matriz[i][j]);                
-            }
-            System.out.println();
-        }
-    }
-
-    public static int[][] matrizNovaInt (int n_linhas, int n_colunas){
-        int [][] matriz = new int[n_linhas][n_colunas];
-        return matriz;
-    }
+    // public static void matrizPrint(int [][]matriz){
+    //     for (int i = 0; i<matriz.length; i++){
+    //         for (int j=0; j<matriz[0].length;j++){
+    //             System.out.printf("%d",matriz[i][j]);                
+    //         }
+    //         System.out.println();
+    //     }
+    // }
 
 
     public static int[][] matrizNovaInt(int colunas, int linhas){
